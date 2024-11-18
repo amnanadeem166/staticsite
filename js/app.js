86c453805728ac159668526d77e25227cdf3f580
amnanadeem166 <amnanadeem166@gmail.com>
2024-11-18 14:31:38 +0500

    Normalize line endings

---
+++ js/app.js
@@ -0,0 +1,5 @@
+// app.js
+document.getElementById('changeContentButton').addEventListener('click', function() {
+    document.querySelector('main p').textContent = 'You clicked the button!';
+  });
+  
